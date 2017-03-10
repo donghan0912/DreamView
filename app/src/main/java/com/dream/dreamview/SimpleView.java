dabc23a67be274678ea5a490668aad3d402961ed
hpu2010 <donghan2010@126.com>
2017-03-10 14:18:31 +0800

    change:

---
+++ app/src/main/java/com/dream/dreamview/SimpleView.java
@@ -0,0 +1,55 @@
+package com.dream.dreamview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/3/9.
+ */
+
+public class SimpleView extends View {
+
+
+    public SimpleView(Context context) {
+        super(context);
+    }
+
+    public SimpleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public SimpleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+            super(context, attrs, defStyleAttr, defStyleRes);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(70);
+        String text = "hellow View";
+        canvas.drawText(text, 0, getHeight() / 2, paint);
+    }
+}
---
+++ doc/自定义view.md
@@ -0,0 +1,3 @@
+http://www.gcssloop.com/customview/CustomViewIndex/
+
+http://blog.csdn.net/guolin_blog/article/details/17357967
