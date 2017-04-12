1e5dc886fd0588f9dd9c78dd6a014ff378fecb0e
hpu2010 <donghan2010@126.com>
2017-04-12 15:57:39 +0800

    change: 完善

--- app/src/main/java/com/dream/dreamview/MainActivity.java
+++ app/src/main/java/com/dream/dreamview/MainActivity.java
@@ -3,27 +3,52 @@ package com.dream.dreamview;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private MultiStatusLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final MultiStatusLayout layout = (MultiStatusLayout) findViewById(R.id.status_layout);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        layout.showLoadingView();
-                    }
-                });
-            }
-        }, 5000);
+        mLayout = (MultiStatusLayout) findViewById(R.id.status_layout);
+       findViewById(R.id.btn_1).setOnClickListener(this);
+       findViewById(R.id.btn_2).setOnClickListener(this);
+       findViewById(R.id.btn_3).setOnClickListener(this);
+       findViewById(R.id.btn_4).setOnClickListener(this);
+       findViewById(R.id.btn_5).setOnClickListener(this);
+       findViewById(R.id.btn_6).setOnClickListener(this);
+       findViewById(R.id.btn_7).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_1:
+                mLayout.showContentView();
+                break;
+            case R.id.btn_2:
+                mLayout.showErrorView();
+                break;
+            case R.id.btn_3:
+                mLayout.showLoadingView();
+                break;
+            case R.id.btn_4:
+                mLayout.showNoNetworkView();
+                break;
+            case R.id.btn_5:
+                mLayout.showLoginView();
+                break;
+            case R.id.btn_6:
+                mLayout.showTimeOutView();
+                break;
+            case R.id.btn_7:
+                mLayout.showEmptyView();
+                break;
+        }
+    }
 }
--- app/src/main/java/com/dream/dreamview/MultiStatusLayout.java
+++ app/src/main/java/com/dream/dreamview/MultiStatusLayout.java
@@ -15,15 +15,23 @@ import android.widget.FrameLayout;
  */
 
 public class MultiStatusLayout extends FrameLayout {
-    private static final int CONTENT_VIEW = -10;
-    private static final int LOADING_VIEW = -11;
+    private static final int CONTENT_VIEW = 0x0001;
+    private static final int LOADING_VIEW = 0x0003;
 
-    private View mContentView;
-    private View mCurrentView;
     private int mLoadingLayout;
+    private int mEmptyLayout;
+    private int mErrorLayout;
+    private int mLoginLayout;
+    private int mNoNetworkLayout;
+    private int mTimeOutLayout;
     private LayoutInflater mInflater;
+    private View mContentView;
     private View mLoadingView;
-    private int mStatus = CONTENT_VIEW;
+    private View mEmptyView;
+    private View mErrorView;
+    private View mLoginView;
+    private View mNoNetworView;
+    private View mTimeOutView;
 
     public MultiStatusLayout(@NonNull Context context) {
         this(context, null);
@@ -36,7 +44,12 @@ public class MultiStatusLayout extends FrameLayout {
     public MultiStatusLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MultiStatusLayout);
-        mLoadingLayout = a.getResourceId(R.styleable.MultiStatusLayout_loadingLayout, R.layout.loading_layout);
+        mLoadingLayout = a.getResourceId(R.styleable.MultiStatusLayout_loadingLayout, R.layout.layout_loading);
+        mEmptyLayout = a.getResourceId(R.styleable.MultiStatusLayout_emptyLayout, R.layout.layout_empty);
+        mErrorLayout = a.getResourceId(R.styleable.MultiStatusLayout_errorLayout, R.layout.layout_error);
+        mLoginLayout = a.getResourceId(R.styleable.MultiStatusLayout_loginLayout, R.layout.layout_login);
+        mNoNetworkLayout = a.getResourceId(R.styleable.MultiStatusLayout_noNetworkLayout, R.layout.layout_no_network);
+        mTimeOutLayout = a.getResourceId(R.styleable.MultiStatusLayout_timeOutLayout, R.layout.layout_time_out);
         a.recycle();
     }
 
@@ -45,29 +58,82 @@ public class MultiStatusLayout extends FrameLayout {
         super.onFinishInflate();
         mInflater = LayoutInflater.from(getContext());
         mContentView = getChildAt(0);
-        mCurrentView = mContentView;
         showContentView();
     }
 
     public void showContentView() {
-        mStatus = CONTENT_VIEW;
-        showViewByStatus(mStatus);
+        showCurrentView(mContentView);
     }
 
     public void showLoadingView() {
-        mStatus = LOADING_VIEW;
-        mLoadingView = mInflater.inflate(mLoadingLayout, null);
-        mCurrentView = mLoadingView;
-        addView(mLoadingView);
-        showViewByStatus(mStatus);
+        if (mLoadingView == null) {
+            mLoadingView = mInflater.inflate(mLoadingLayout, null);
+            addView(mLoadingView);
+        }
+        showCurrentView(mLoadingView);
     }
 
-    private void showViewByStatus(int status) {
+    public void showEmptyView() {
+        if (mEmptyView == null) {
+            mEmptyView = mInflater.inflate(mEmptyLayout, null);
+            addView(mEmptyView);
+        }
+        showCurrentView(mEmptyView);
+    }
+
+    public void showErrorView() {
+        if (mErrorView == null) {
+            mErrorView = mInflater.inflate(mErrorLayout, null);
+            addView(mErrorView);
+        }
+        showCurrentView(mErrorView);
+    }
+
+    public void showLoginView() {
+        if (mLoginView == null) {
+            mLoginView = mInflater.inflate(mLoginLayout, null);
+            addView(mLoginView);
+        }
+        showCurrentView(mLoginView);
+    }
+
+    public void showNoNetworkView() {
+        if (mNoNetworView == null) {
+            mNoNetworView = mInflater.inflate(mNoNetworkLayout, null);
+            addView(mNoNetworView);
+        }
+        showCurrentView(mNoNetworView);
+    }
+
+    public void showTimeOutView() {
+        if (mTimeOutView == null) {
+            mTimeOutView = mInflater.inflate(mTimeOutLayout, null);
+            addView(mTimeOutView);
+        }
+        showCurrentView(mTimeOutView);
+    }
+
+    private void showCurrentView(View currentView) {
         if (mContentView != null) {
-            mContentView.setVisibility(mCurrentView == mContentView ? VISIBLE : GONE);
+            mContentView.setVisibility(currentView == mContentView ? VISIBLE : GONE);
         }
         if (mLoadingView != null) {
-            mLoadingView.setVisibility(mCurrentView == mLoadingView ? VISIBLE : GONE);
+            mLoadingView.setVisibility(currentView == mLoadingView ? VISIBLE : GONE);
+        }
+        if (mEmptyView != null) {
+            mEmptyView.setVisibility(currentView == mEmptyView ? VISIBLE : GONE);
+        }
+        if (mErrorView != null) {
+            mErrorView.setVisibility(currentView == mErrorView ? VISIBLE : GONE);
+        }
+        if (mLoginView != null) {
+            mLoginView.setVisibility(currentView == mLoginView ? VISIBLE : GONE);
+        }
+        if (mNoNetworView != null) {
+            mNoNetworView.setVisibility(currentView == mNoNetworView ? VISIBLE : GONE);
+        }
+        if (mTimeOutView != null) {
+            mTimeOutView.setVisibility(currentView == mTimeOutView ? VISIBLE : GONE);
         }
     }
 }
