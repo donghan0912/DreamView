16d7a561ba91a3a4e3720ad50038091e3feb591a
hpu2010 <donghan2010@126.com>
2017-08-24 14:25:44 +0800

    kotlin activity

--- app/src/main/java/com/dream/dreamview/module/room/RoomDBActivity.kt
+++ app/src/main/java/com/dream/dreamview/module/room/RoomDBActivity.kt
@@ -5,21 +5,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.dream.dreamview.R
 import com.dream.dreamview.base.NavBaseActivity
 import com.dream.dreamview.module.room.adapter.RoomItem
+import com.dream.dreamview.util.ToastUtil
 import com.hpu.baserecyclerviewadapter.BaseRecyclerViewAdapter
 
 /**
  * Created on 2017/8/22.
  */
 class RoomDBActivity : NavBaseActivity() {
-    private lateinit var recyclerView : RecyclerView
+    private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: BaseRecyclerViewAdapter<RoomItem>
 
     companion object {
         @JvmStatic
-        fun start(context : Context) {
+        fun start(context: Context) {
             val intent = Intent(context, RoomDBActivity::class.java)
             context.startActivity(intent)
         }
@@ -37,6 +39,14 @@ class RoomDBActivity : NavBaseActivity() {
         recyclerView.adapter = adapter
         val list = ArrayList<RoomItem>()
         list.add(RoomItem("数据库"))
+        list.add(RoomItem("数据库"))
+        list.add(RoomItem("数据库"))
+        list.add(RoomItem("数据库"))
+        list.add(RoomItem("数据库"))
+        list.add(RoomItem("数据库"))
         adapter.data = list
+        adapter.setOnItemClickListener { p0, p1 ->
+            ToastUtil.showShortToast(applicationContext, "tt")
+        }
     }
 }
