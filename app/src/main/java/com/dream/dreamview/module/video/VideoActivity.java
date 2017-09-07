b901337d76ffd6824112ac706babfddbb6c2c0f9
董晗 <donghan2010@126.com>
2017-09-08 00:46:13 +0800

    解决视频自动横屏后，点击缩小后，视频还是横屏显示

--- app/src/main/java/com/dream/dreamview/module/video/VideoActivity.java
+++ app/src/main/java/com/dream/dreamview/module/video/VideoActivity.java
@@ -1,14 +1,17 @@
 package com.dream.dreamview.module.video;
 
+import android.app.KeyguardManager;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.view.KeyEvent;
 import android.view.OrientationEventListener;
+import android.view.WindowManager;
 
 import com.dream.dreamview.R;
 import com.dream.dreamview.base.NavBaseActivity;
@@ -110,11 +113,27 @@ public class VideoActivity extends NavBaseActivity {
     }
 
     private OrientationEventListener mOrientationListener;
+    private int oldScreenOrientation;
     // TODO 处理屏幕旋转
     private void t() {
+        // TODO 打开关闭屏幕常亮功能，ExoPlayerView需要对外暴露一个视频开始结束Listener
+        // TODO 配置一个是否常亮开关
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         mOrientationListener = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
             @Override
             public void onOrientationChanged(int orientation) {
+                try {
+                    int flag = Settings.System.getInt(getContentResolver(), Settings.System.ACCELEROMETER_ROTATION);
+                    if (flag == 0) { // 0 表示手机自动旋转功能未打开 1：表示打开
+                        return;
+                    }
+                } catch (Settings.SettingNotFoundException e) {
+                    e.printStackTrace();
+                }
+                if (orientation == OrientationEventListener.ORIENTATION_UNKNOWN) {
+                    return;  //手机平放时，检测不到有效的角度
+                }
                 //只检测是否有四个角度的改变
                 if (orientation > 350 || orientation < 10) { //0度
                     orientation = 0;
@@ -126,11 +145,20 @@ public class VideoActivity extends NavBaseActivity {
                     orientation = 270;
                 }
                 if (orientation == 90) {
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                    if (oldScreenOrientation != orientation) {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                        oldScreenOrientation = orientation;
+                    }
                 } else if (orientation == 270) {
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                    if (oldScreenOrientation != orientation) {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                        oldScreenOrientation = orientation;
+                    }
                 } else if (orientation == 0) {
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                    if (oldScreenOrientation != orientation) {
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                        oldScreenOrientation = orientation;
+                    }
                 }
             }
         };
