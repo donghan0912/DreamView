db18e04fba3dbc5c3417bf35bf84c51b8594ed34
hpu2010 <donghan2010@126.com>
2018-08-10 18:17:55 +0800

    nothing to commit

--- app/src/main/java/com/dream/dreamview/test/Demo.java
+++ app/src/main/java/com/dream/dreamview/test/Demo.java
@@ -24,9 +24,12 @@ public class Demo {
       System.out.println(current.item);
       current = current.next;
     }
-    System.out.println("ok");
-    int[] ints = {4, 1, 6};
-    funBubbleSort(ints);
+//    System.out.println("ok");
+    int[] ints = {4, 1, 6, 3, 9, 8};
+    int[] ints2 = {1, 11, 22, 33, 44, 55, 66, 77, 88};
+//    funBubbleSort(ints);
+//    selectSort(ints);
+    search(ints2, 88);
   }
 
   private static void init(Node node) {
@@ -80,23 +83,45 @@ public class Demo {
   }
 
   // 选择排序
-  static void select(int[] array) {
-
+  static void selectSort(int[] array) {
     for (int i = 0; i < array.length - 1; i++) {
-      int mink = i;
-      // 每次从未排序数组中找到最小值的坐标
+      int mark = i;
+      // 第一步，从未排序的数组中，找到最小值的角标
       for (int j = i + 1; j < array.length; j++) {
-        if (array[j] < array[mink]) {
-          mink = j;
+        if (array[j] < array[mark]) {
+          mark = j;
         }
       }
-      // 将最小值放在最前面
-      if (mink != i) {
-        int temp = array[mink];
-        array[mink] = array[i];
+      // 第二步，将最小值放到前面
+      if (mark != i) {
+        int temp = array[mark];
+        array[mark] = array[i];
         array[i] = temp;
       }
     }
+    System.out.print("选择排序");
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + "==");
+    }
+  }
+
+  // 二分查找
+  static void search(int[] array, int value) {
+    int low = 0;
+    int high = array.length - 1;
+    int tttt = -1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (value == array[mid]) {
+        tttt = mid;
+        break;
+      } else if (value < array[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    System.out.print(tttt + "======");
   }
 
 }
